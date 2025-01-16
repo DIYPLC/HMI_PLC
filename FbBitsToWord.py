945a8a515702f397e66b74c03b87551a12a3e476
VA <96442443+DIYPLC@users.noreply.github.com>
2025-01-16 09:22:40 +0300

    pycharm rulit

---
+++ FbBitsToWord.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 18 apr 2024
+# Windows 10 pro x64
+# Python 3.9.0
+# PyCharm Community Edition 2024.1
+
+# Преобразование 16 бит в слово.
+#      DbBitsToWord
+#    +--------------+
+#    | FbBitsToWord |
+# ->-|In0        Out|->-
+# ->-|In1           |
+# ->-|In2           |
+# ->-|In3           |
+# ->-|In4           |
+# ->-|In5           |
+# ->-|In6           |
+# ->-|In7           |
+# ->-|In8           |
+# ->-|In9           |
+# ->-|In10          |
+# ->-|In11          |
+# ->-|In12          |
+# ->-|In13          |
+# ->-|In14          |
+# ->-|In15          |
+#    +--------------+
+
+
+class FbBitsToWord(object):
+    """Преобразование 16 бит в слово."""
+
+    def __init__(self) -> None:
+        """Конструктор."""
+        # Входные переменные, сохраняемые.
+        self.In0: bool = False
+        self.In1: bool = False
+        self.In2: bool = False
+        self.In3: bool = False
+        self.In4: bool = False
+        self.In5: bool = False
+        self.In6: bool = False
+        self.In7: bool = False
+        self.In8: bool = False
+        self.In9: bool = False
+        self.In10: bool = False
+        self.In11: bool = False
+        self.In12: bool = False
+        self.In13: bool = False
+        self.In14: bool = False
+        self.In15: bool = False
+        # Выходные переменные, сохраняемые.
+        self.Out: int = 0
+        # Внутренние переменные, сохраняемые.
+        return
+
+    def __call__(self) -> None:
+        """Вызов экземпляра  DbBitsToWord()."""
+        self.Out = 0
+        if self.In0:
+            self.Out = (self.Out | 0b0000_0000_0000_0001)
+        if self.In1:
+            self.Out = (self.Out | 0b0000_0000_0000_0010)
+        if self.In2:
+            self.Out = (self.Out | 0b0000_0000_0000_0100)
+        if self.In3:
+            self.Out = (self.Out | 0b0000_0000_0000_1000)
+        if self.In4:
+            self.Out = (self.Out | 0b0000_0000_0001_0000)
+        if self.In5:
+            self.Out = (self.Out | 0b0000_0000_0010_0000)
+        if self.In6:
+            self.Out = (self.Out | 0b0000_0000_0100_0000)
+        if self.In7:
+            self.Out = (self.Out | 0b0000_0000_1000_0000)
+        if self.In8:
+            self.Out = (self.Out | 0b0000_0001_0000_0000)
+        if self.In9:
+            self.Out = (self.Out | 0b0000_0010_0000_0000)
+        if self.In10:
+            self.Out = (self.Out | 0b0000_0100_0000_0000)
+        if self.In11:
+            self.Out = (self.Out | 0b0000_1000_0000_0000)
+        if self.In12:
+            self.Out = (self.Out | 0b0001_0000_0000_0000)
+        if self.In13:
+            self.Out = (self.Out | 0b0010_0000_0000_0000)
+        if self.In14:
+            self.Out = (self.Out | 0b0100_0000_0000_0000)
+        if self.In15:
+            self.Out = (self.Out | 0b1000_0000_0000_0000)
+        return
+
+    def __del__(self) -> None:
+        """Деструктор."""
+        del self
+        return
+
+    def set_in0(self, value: bool) -> None:
+        self.In0 = bool(value)
+        return
+
+    def set_in1(self, value: bool) -> None:
+        self.In1 = bool(value)
+        return
+
+    def set_in2(self, value: bool) -> None:
+        self.In2 = bool(value)
+        return
+
+    def set_in3(self, value: bool) -> None:
+        self.In3 = bool(value)
+        return
+
+    def set_in4(self, value: bool) -> None:
+        self.In4 = bool(value)
+        return
+
+    def set_in5(self, value: bool) -> None:
+        self.In5 = bool(value)
+        return
+
+    def set_in6(self, value: bool) -> None:
+        self.In6 = bool(value)
+        return
+
+    def set_in7(self, value: bool) -> None:
+        self.In7 = bool(value)
+        return
+
+    def set_in8(self, value: bool) -> None:
+        self.In8 = bool(value)
+        return
+
+    def set_in9(self, value: bool) -> None:
+        self.In9 = bool(value)
+        return
+
+    def set_in10(self, value: bool) -> None:
+        self.In10 = bool(value)
+        return
+
+    def set_in11(self, value: bool) -> None:
+        self.In11 = bool(value)
+        return
+
+    def set_in12(self, value: bool) -> None:
+        self.In12 = bool(value)
+        return
+
+    def set_in13(self, value: bool) -> None:
+        self.In13 = bool(value)
+        return
+
+    def set_in14(self, value: bool) -> None:
+        self.In14 = bool(value)
+        return
+
+    def set_in15(self, value: bool) -> None:
+        self.In15 = bool(value)
+        return
+
+    def get_out(self) -> int:
+        return int(self.Out)
+
+
+def unit_test() -> None:
+    print("Unit test start")
+    from FbWordToBits import FbWordToBits
+    word_to_bits = FbWordToBits()
+    bits_to_word = FbBitsToWord()
+    error_flag = False
+    for i in range(65536):
+        word_to_bits.In = i
+        word_to_bits()
+        bits_to_word.In0 = word_to_bits.Out0
+        bits_to_word.In1 = word_to_bits.Out1
+        bits_to_word.In2 = word_to_bits.Out2
+        bits_to_word.In3 = word_to_bits.Out3
+        bits_to_word.In4 = word_to_bits.Out4
+        bits_to_word.In5 = word_to_bits.Out5
+        bits_to_word.In6 = word_to_bits.Out6
+        bits_to_word.In7 = word_to_bits.Out7
+        bits_to_word.In8 = word_to_bits.Out8
+        bits_to_word.In9 = word_to_bits.Out9
+        bits_to_word.In10 = word_to_bits.Out10
+        bits_to_word.In11 = word_to_bits.Out11
+        bits_to_word.In12 = word_to_bits.Out12
+        bits_to_word.In13 = word_to_bits.Out13
+        bits_to_word.In14 = word_to_bits.Out14
+        bits_to_word.In15 = word_to_bits.Out15
+        bits_to_word()
+        # print(hex(i), hex(bits_to_word.Out), hex(word_to_bits.In))
+        if bits_to_word.Out != word_to_bits.In:
+            error_flag = True
+    if error_flag:
+        print("Test ERROR")
+    else:
+        print("Test OK")
+    return
+
+
+if __name__ == "__main__":
+    unit_test()
+    input("press any key for exit...")
+
+# @COPYLEFT ALL WRONGS RESERVED :)
+# Author: VA
+# Contacts: DIY.PLC.314@gmail.com
+# Date start LIB_PLC: 2014
+# License: GNU GPL-2.0-or-later
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# https://www.youtube.com/watch?v=n1F_MfLRlX0
+# https://www.youtube.com/@DIY_PLC
+# https://github.com/DIYPLC/LIB_PLC
+# https://oshwlab.com/diy.plc.314/PLC_HW1_SW1
+# https://3dtoday.ru/3d-models/mechanical-parts/body/korpus-na-din-reiku
+# https://t.me/DIY_PLC
+
+# Спасибо за лекции.
+# https://www.youtube.com/@unx7784/playlists
+# https://www.youtube.com/@tkhirianov/playlists
---
+++ FbFilterA.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Фильтр апериодический.
+#      DbFilterA
+#    +-----------+
+#    | FbFilterA |
+# ->-|In      Out|->-
+#   -|Tf         |
+#   -|Ts         |
+#    +-----------+
+
+class FbFilterA():
+    #Входные переменные, сохраняемые.
+    In = float(0.0); #Входной сигнал до фильтрации.
+    Tf = float(1.0); #Постоянная времени фильтра [с].
+    Ts = float(0.1); #Шаг дискретизации по времени [с].
+    #Выходные переменные, сохраняемые.
+    Out = float(0.0); #Выходной сигнал после фильтрации.
+    #Фильтрация
+    def execute(self):
+        #Внутренние переменные, не сохраняемые.
+        Tmp = float(0.0);
+        #W(s) = 1/(1+Tf*s) при Ts->0.
+        if (self.Tf <= 0.0):
+            self.Out = self.In;
+        else:
+            Tmp = (self.In - self.Out) / self.Tf;
+            self.Out = self.Out + Tmp * self.Ts;
+        return
+
+def Unit_test():
+    DbFilterA1 = FbFilterA()
+    DbFilterA1.In = 1.0
+    Timer1 = 0.0
+    for i in range(15):
+        print("T:",Timer1,"Out:",DbFilterA1.Out)
+        DbFilterA1.execute()
+        Timer1 = Timer1 + 0.1
+
+if (__name__ == '__main__'):
+    Unit_test()
+    input("press any key ")
+
+# @COPYLEFT ALL WRONGS RESERVED :)
+# Author: VA
+# Contacts: DIY.PLC.314@gmail.com
+# Date start LIB_PLC: 2014
+# License: GNU GPL-2.0-or-later
+# https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# https://www.youtube.com/watch?v=n1F_MfLRlX0
+# https://www.youtube.com/@DIY_PLC
+# https://github.com/DIYPLC/LIB_PLC
+# https://oshwlab.com/diy.plc.314/PLC_HW1_SW1
+# https://3dtoday.ru/3d-models/mechanical-parts/body/korpus-na-din-reiku
+# https://t.me/DIY_PLC
