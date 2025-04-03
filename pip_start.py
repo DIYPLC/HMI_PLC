4e512c6e49fc982b2fc7cbe9ff0afad369c66cb3
VA <96442443+DIYPLC@users.noreply.github.com>
2025-04-03 21:41:25 +0300

    Add files via upload

--- pip_start.py
+++ pip_start.py
@@ -7,41 +7,17 @@ print("Загружаем библиотеки через интернет.")
 
 if (os.name == 'nt'):
     print("Программа запущена на Windows.")
-    print("python.exe -m pip install --upgrade pip")
     os.system("python.exe -m pip install --upgrade pip")
-
 
 if (os.name == 'posix'):
     print("Программа запущена на Linux.")
-    print("python3 -m pip install --upgrade pip")
-    os.system("sudo apt-get install python3-pip")
-    os.system("python.exe -m pip install --upgrade pip")
-    os.system("sudo apt-get update")
-    os.system("sudo apt-get upgrade")
-    os.system("sudo apt-get install gcc")
-    os.system("sudo apt-get install g++")
-    os.system("sudo apt-get install make")
-    os.system("sudo apt-get install git")
-    os.system("sudo apt-get install vim")
-    os.system("sudo apt-get install nmap")
-    os.system("sudo apt-get install net-tools")
-    os.system("sudo apt-get install htop")
-    os.system("sudo apt-get install cmatrix")
-    os.system("sudo apt-get install mc")
-    os.system("sudo apt-get install tmux")
-    os.system("sudo apt-get install dosbox")
-    os.system("git clone https://github.com/DIYPLC/LIB_PLC.git")
-    os.system("git clone https://github.com/DIYPLC/DIY_PLC.git")
-    os.system("git clone https://github.com/DIYPLC/HMI_PLC.git")
-    os.system("git clone https://github.com/DIYPLC/Tests.git")
-
+    os.system("sudo apt update")
 
 print("Библиотека MODBUS RTU MASTER")
 try:
     import minimalmodbus
 except:
     os.system("pip3 install minimalmodbus")
-
 
 print("Библиотека OPC UA CLIENT / SERVER")
 try:
@@ -52,14 +28,12 @@ except:
     print("pip3 install opcua-client")
     os.system("pip3 install opcua-client")
 
-
 print("Библиотека для подключение к MS SQL SERVER")
 try:
     import pyodbc
 except:
     print("pip3 install pyodbc")
     os.system("pip3 install pyodbc")
-
 
 print("Библиотека для доступа к RS232/RS485")
 try:
@@ -68,7 +42,6 @@ except:
     print("pip3 install pyserial")
     os.system("pip3 install pyserial")
 
-
 print("Библиотека для подключения к PLC SIEMENS S7-300")
 try:
     import snap7
@@ -76,16 +49,6 @@ except:
     print("pip install python-snap7")
     #print("COPY FILE!!! C:\Windows\System32\snap7.dll C:\Windows\System32\snap7.lib")
     os.system("pip install python-snap7")
-
-
-print("Библиотека математики и графиков")
-try:
-    import numpy
-    import matplotlib.pyplot
-except:
-    print("pip3 install matplotlib")
-    os.system("pip3 install matplotlib")
-
 
 input("press any key for exit...")
 
