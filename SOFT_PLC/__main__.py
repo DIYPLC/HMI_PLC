119e2fa28b7ddb9f8e05acb751b0c0bb710d51a4
VA <96442443+DIYPLC@users.noreply.github.com>
2025-04-08 20:56:56 +0300

    Add files via upload

--- SOFT_PLC/__main__.py
+++ SOFT_PLC/__main__.py
@@ -6,15 +6,13 @@ SOFT PLC
 Python 3.12.2
 Windows 10 Pro x64
 Ubuntu 20.04.6 LTS + Python 3.8.10
-15 jan 2025
+08-04-2025
 """
 
 #import tkinter # TODO in ubuntu not work
 import time
-import socket
-import struct
-import array
-import ctypes
+import socket,struct,array,ctypes
+import gc,importlib,os
 from RTC import Rtc
 from Write_to_Error_file import write_to_error_file
 
@@ -24,62 +22,89 @@ def error_generator():
 
 
 class GlobalVar(object):
-
+    
     def __init__(self) -> None:
-        """ GLOBAL VAR """
+        """ SYSTEM VARIABLES """
         self.time_sample_ns = int(0)
         self.Reset = bool(True)
-        """ TODO MODBUS TCP CLIENT """
-        self.ADR1_MW0 = int(0)  # MODBUS TCP Holding register int16
-        """ USER VARIABLES """
-        self.user_timer_1_ns = int(0)
-        """ GLOBAL CLASS """
-        try:
-            self.rtc = Rtc()
-        except BaseException as error:
-            write_to_error_file("ERROR GlobalVar __init__ Rtc() " + str(error))
-
-    def __call__(self) -> None:
-        self.Reset = False
-        self.rtc()
-        self.time_sample_ns = self.rtc.time_sample_ns
+        self.Stop_PLC = bool(False)
         return
-
+    
     def __del__(self) -> None:
         del self
         return
 
 
-def task_cyclic(reset: bool = False, time_sample_ns: int = 0) -> None: # LIB_PLC style.
-    GV.user_timer_1_ns = GV.user_timer_1_ns + time_sample_ns
-    user_timer_1_s = float(GV.user_timer_1_ns / (10**9))
-    print("user_timer_1_s", user_timer_1_s)
-    return
+class Task_cyclic(object):
+    
+    def __init__(self) -> None:
+        self.time_sample_ns = int(0)
+        self.reset = bool(False)
+        self.user_timer_1_ns = int(0)
+        self.user_timer_1_s = float(0.0)
+        return
+    
+    def __call__(self) -> None:
+        self.user_timer_1_ns = self.user_timer_1_ns + self.time_sample_ns
+        self.user_timer_1_s = float(self.user_timer_1_ns / (10**9))
+        print("user_timer_1_s", self.user_timer_1_s)
+        return
+    
+    def __del__(self) -> None:
+        del self
+        return
 
 
 def main() -> None:  # GCC style.
-    task_cyclic(reset=GV.Reset, time_sample_ns=GV.rtc.time_sample_ns)
+    rtc()
+    GV.time_sample_ns = rtc.time_sample_ns
+    GV.Reset = True
+    Task_cyclic1.time_sample_ns = GV.time_sample_ns
+    Task_cyclic1.reset = True
+    Task_cyclic1()
     while True:
         time.sleep(0.5)  # TODO DEBUG
-        GV()  # Update sample time and reset flag
-        task_cyclic(reset=GV.Reset, time_sample_ns=GV.rtc.time_sample_ns)
+        rtc()
+        GV.time_sample_ns = rtc.time_sample_ns
+        GV.Reset = False
+        Task_cyclic1.time_sample_ns = GV.time_sample_ns
+        Task_cyclic1.reset = False
+        Task_cyclic1()
+        if GV.Stop_PLC:
+            write_to_error_file("OK SOFT PLC STOP")
+            break
+
+
+def auto_restart_plc() -> None:
+    """" Этот метод работает но приводит к утечке памяти. """
+    """ Возможно лучше перезагрузить OS. """
+    print("auto_restart_plc()")
+    if (os.name == 'posix'):
+        os.system("python3 __main__.py")
+    else: # os.name == 'nt'
+        os.system("__main__.py")
+    return
 
 
 if __name__ == "__main__":
-    debug_mode: bool = False  # TODO DEBUG
-    if debug_mode:
+    try:
+        write_to_error_file("OK SOFT PLC START")
+        rtc = Rtc()
         GV = GlobalVar()
+        Task_cyclic1 = Task_cyclic()
         main()
-    else:
-        while True:
-            try:
-                write_to_error_file("OK SOFT PLC START")
-                GV = GlobalVar()
-                main()
-            except BaseException as error:
-                print("ERROR delay 10s")
-                write_to_error_file("ERROR __main__.py " + str(error))
-            time.sleep(10)  # second
+    except BaseException as error:
+        print("ERROR delay")
+        write_to_error_file("ERROR __main__.py " + str(error))
+        del rtc
+        del GV
+        del Task_cyclic1
+        del main
+        importlib.reload(socket)
+        gc.collect()
+        time.sleep(5)
+        auto_restart_plc()
+
 
 # @COPYLEFT ALL WRONGS RESERVED :)
 # Author: VA
