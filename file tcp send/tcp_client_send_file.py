cb76d7f1cf73f68e627f982a99a19747879a4c7f
VA <96442443+DIYPLC@users.noreply.github.com>
2025-11-27 21:14:27 +0300

    Add files via upload

---
+++ file tcp send/tcp_client_send_file.py
@@ -0,0 +1,22 @@
+"""
+TCP Client for transmit file.
+https://techexpert.tips/python/python-file-transfer-using-sockets/
+"""
+import socket
+
+
+def send_file(file_transmit: str = "tx_file.txt", ip_client: str = "127.0.0.1", tcp_port: int = 50002):
+    print("Start send file.")
+    buffer_size = 1024  # Buffer size for sending
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
+        socket_tcp.connect((ip_client, tcp_port))
+        with open(file_transmit, 'rb') as file_read:
+            while True:
+                data_transmit = file_read.read(buffer_size)
+                if not data_transmit:
+                    break
+                socket_tcp.sendall(data_transmit)
+        print("File send successfully!")
+
+
+send_file(file_transmit="file.txt", ip_client="192.168.88.18")
---
+++ file tcp send/tcp_server_recive_file.py
@@ -0,0 +1,27 @@
+"""
+TCP Server for receive file.
+https://techexpert.tips/python/python-file-transfer-using-sockets/
+"""
+
+import socket
+
+
+def receive_file(file_receive: str = "rx_file.txt", ip_server: str = "0.0.0.0", tcp_port: int = 50002):
+    buffer_size = 1024  # Buffer size for receiving
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
+        socket_tcp.bind((ip_server, tcp_port))
+        socket_tcp.listen(1)  # Number of TCP clients
+        print("Waiting for connection ip=", ip_server, ",port=", tcp_port)
+        connection_server, ip_adr = socket_tcp.accept()
+        with connection_server:
+            print("Connected by", ip_adr)
+            with open(file_receive, 'wb') as file_write:
+                while True:
+                    data_receive = connection_server.recv(buffer_size)
+                    if not data_receive:
+                        break
+                    file_write.write(data_receive)
+            print("File received successfully!")
+
+
+receive_file(file_receive="file.txt")
